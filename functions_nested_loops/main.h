bec9e49149077d9b2dbdbb725fa3e5254d53dad2
Aymenzzzz <7462@holbertonstudents.com>
2023-10-19 04:39:42 -0700

    ieiei

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _putchar(char c);
+void cint _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -7,3 +7,6 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
