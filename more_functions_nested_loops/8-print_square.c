f6ad44b16fc4bc170fb56d38ea31da0cbd994461
Aymenzzzz <7462@holbertonstudents.com>
2023-10-20 06:52:15 -0700

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_diagonal - Write a function that checks for uppercase character.
- * @n : number
+ * print_square - print a square
+ * @size: size square
+ * Return: Alway 0.
  */
-void print_diagonal(int n)
-
+void print_square(int size)
 {
-	int i;
+	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		putchar('\n');
 	}
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < size; i++)
 	{
-		int j;
-
-		for (j = 1; j < i; j++)
+		for (j = 0; j < size; j++)
 		{
-			putchar(' ');
+			putchar('#');
 		}
-
-		putchar('\\');
 		putchar('\n');
 	}
 }
